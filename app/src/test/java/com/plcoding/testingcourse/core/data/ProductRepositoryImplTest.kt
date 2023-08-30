31b54265939bb32354ba1a06edb03f526d40cd47
Chris Athanas <realityexpander@gmail.com>
2023-08-29 23:32:41 -0500

    Add mock ws example

--- app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
@@ -4,24 +4,23 @@ import assertk.assertThat
 import assertk.assertions.isTrue
 import com.plcoding.testingcourse.core.domain.AnalyticsLogger
 import com.plcoding.testingcourse.core.domain.LogParam
-import com.plcoding.testingcourse.core.domain.Product
-import com.plcoding.testingcourse.core.domain.ProductRepository
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkConstructor
 import io.mockk.verify
 import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import retrofit2.HttpException
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+
 
 internal class ProductRepositoryImplTest {
 
@@ -49,6 +48,25 @@ internal class ProductRepositoryImplTest {
         mockWebServer.enqueue(
             MockResponse().setResponseCode(404)
         )
+//        mockWebServer.enqueue(
+//            MockResponse().setBody("{}").setResponseCode(200)
+//        )
+
+        val dispatcher: Dispatcher = object : Dispatcher() {
+            @Throws(InterruptedException::class)
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                when (request.path) {
+                    "/v1/login/auth/" -> return MockResponse().setResponseCode(200)
+                    "v1/check/version/" -> return MockResponse().setResponseCode(200)
+                        .setBody("version=9")
+
+                    "/v1/profile/info" -> return MockResponse().setResponseCode(200)
+                        .setBody("{\\\"info\\\":{\\\"name\":\"Lucas Albuquerque\",\"age\":\"21\",\"gender\":\"male\"}}")
+                }
+                return MockResponse().setResponseCode(404)
+            }
+        }
+        mockWebServer.dispatcher = dispatcher
 
         val result = repository.purchaseProducts(listOf())
 
